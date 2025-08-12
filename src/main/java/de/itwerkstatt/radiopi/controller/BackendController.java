f8d3277bb6db74644e75ccca8ffad911c9f19ae8
Dominik Sust <dsust@heidler-strichcode.de>
2025-08-12 15:14:14 +0200

    Implement playSender method in BackendController with RadiostationService integration

--- src/main/java/de/itwerkstatt/radiopi/controller/BackendController.java
+++ src/main/java/de/itwerkstatt/radiopi/controller/BackendController.java
@@ -1,5 +1,6 @@
 package de.itwerkstatt.radiopi.controller;
 
+import de.itwerkstatt.radiopi.services.RadiostationService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class BackendController {
 
+    private final RadiostationService radiostationService;
+
     Logger logger = LogManager.getLogger(BackendController.class);
+
+    public BackendController(RadiostationService radiostationService) {
+        this.radiostationService = radiostationService;
+    }
 
     @RequestMapping("/radiostation/play/{id}")
     public ResponseEntity<Void> playSender(@PathVariable("id") String id) {
-        //TODO
-        return ResponseEntity.noContent().build();
+        if (radiostationService.playSender(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @RequestMapping("/radiostation/stop")
