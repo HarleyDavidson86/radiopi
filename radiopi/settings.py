b638bb3fb5eb8fa0a97a1724652b3d18ea1f8f5b
Dominik Sust <dominik.sust@gmail.com>
2025-12-09 20:03:55 +0100

    Add MEDIA_URL setting and update URL patterns for media files

--- radiopi/settings.py
+++ radiopi/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 # Absolute filesystem path to the directory that will hold radiostation logos.
 MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
