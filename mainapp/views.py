8a3f1bd7eabfb0768c87b34fc07e91f8794dc1e2
Dominik Sust <dominik.sust@gmail.com>
2025-12-09 20:03:37 +0100

    Update index template to display radio stations with logos and short names

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import RadioStation
 
 # Create your views here.
 def index(request):
-    return render(request, "mainapp/index.html")
+    stations = RadioStation.objects.all()
+    return render(request, "mainapp/index.html", {"stations": stations})
 
 def inforightbottom(request):
     current_time = datetime.now().strftime("%H:%M")
